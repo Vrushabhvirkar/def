436bbe1c211aee7e965426e79516ce1ffc2d9f56
Vrushabh Virkar <132008478+Vrushabhvirkar@users.noreply.github.com>
2025-02-08 10:41:43 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb+srv://vegcarton08:hrRvAYSe5xpBylhR@bankagentbridgegrievanc.cqdkm.mongodb.net/userdata",
+mongoose.connect("mongodb+srv://vegcarton08:hrRvAYSe5xpBylhR@bankagentbridgegrievanc.cqdkm.mongodb.net/test",
 {useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
